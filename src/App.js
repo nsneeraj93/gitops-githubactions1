9cc859aafc156c352052e8af67c50541a428d8df
nsneeraj93 <83461963+nsneeraj93@users.noreply.github.com>
2025-11-22 13:40:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <div style={{ fontSize: 30 }}>
           Version{' '}
-          <span style={{ color: 'red' }}>First commit by Vijay....!!!</span>
+          <span style={{ color: 'red' }}>First commit by Neeraj sangevieya....!!!</span>
         </div>
       </header>
     </div>
